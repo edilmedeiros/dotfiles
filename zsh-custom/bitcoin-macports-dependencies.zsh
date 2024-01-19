9e48a4b51e4543eb11acfe985672afb68846a81c
Edil Medeiros <jose.edil@gmail.com>
2024-01-18 23:17:01 -0300

    Move oh-my-zsh custom folder to the repo

---
+++ zsh-custom/bitcoin-macports-dependencies.zsh
@@ -0,0 +1,22 @@
+# Add variables to build bitcoin using macports dependencies
+
+export CPPFLAGS="$CPPFLAGS -isystem /opt/local/include"
+export LIBS="-LIBS -L/opt/local/lib"
+
+# libevent (mandatory)
+export EVENT_CPPFLAGS="-I/opt/local/include"
+export EVENT_LIBS="-I/opt/local/lib -levent"
+
+# Berkeley-db (optional)
+export BDB_CFLAGS="/opt/local/lib/db48"
+export CPPFLAGS="$CPPFLAGS -I/opt/local/include/db48"
+export LIBS="$LIBS -L/opt/local/lib/db48"
+
+# Miniupnpc (optional)
+export MINIUPNPC_CPPFLAGS="-I/opt/local/include"
+export MINIUPNPC_LIBS="-L/opt/local/lib"
+
+# libmultiprocess (optional)
+export PKG_CONFIG_PATH="/usr/local/lib/pkgconfig"
+export LIBMULTIPROCESS_CFLAGS="-I/usr/local/include"
+export LIBMULTIPROCESS_LIBS="-L/usr/local/lib"
---
+++ zsh-custom/bitcoin-path.zsh
@@ -0,0 +1,3 @@
+# Bitcoin path
+
+export PATH="/Users/jose.edil/2-development/bitcoin/binaries/v26/bin:$PATH"
