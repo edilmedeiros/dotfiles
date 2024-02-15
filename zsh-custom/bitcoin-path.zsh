dc7ecd9119fb1209b54aa14b30beb7d6aa3a7097
Edil Medeiros <jose.edil@gmail.com>
2024-02-15 18:01:38 -0300

    Add lnd paths

--- zsh-custom/bitcoin-path.zsh
+++ zsh-custom/bitcoin-path.zsh
@@ -1,5 +1,7 @@
-# Bitcoin path
-
+# Bitcoin paths
 export PATH="/Users/jose.edil/2-development/bitcoin/binaries/v26/bin:$PATH"
-
 export BTC_DATADIR="/Users/jose.edil/Library/Application Support/Bitcoin"
+
+# LND paths
+export PATH="/Users/jose.edil/2-development/bitcoin/binaries/lnd:$PATH"
+export LND_DATADIR="/Users/jose.edil/Library/Application Support/Lnd"
