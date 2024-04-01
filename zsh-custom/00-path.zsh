47e7093e5d2f7fc003ce3a386a6d1e1119f01fb7
Edil Medeiros <jose.edil@gmail.com>
2024-04-01 16:09:48 -0300

    Move file from old custom folder

---
+++ zsh-custom/00-path.zsh
@@ -0,0 +1,31 @@
+# You can put files here to add functionality separated per file, which
+# will be ignored by git.
+# Files on the custom/ directory will be automatically loaded by the init
+# script, in alphabetical order.
+
+# For example: add yourself some shortcuts to projects you often work on.
+#
+# brainstormr=~/Projects/development/planetargon/brainstormr
+# cd $brainstormr
+#
+
+# Python Shenaningans
+export PATH=$PATH:/Users/jose.edil/Library/Python/3.10/bin
+
+# Circuit Macro
+export M4PATH=$M4PATH:~/.texmf/circuit_macros
+
+# NPM 
+export PATH=~/.npm-global/bin:$PATH
+
+# Gem
+export PATH=/Users/jose.edil/.local/share/gem/ruby/3.3.0/bin:$PATH
+
+# Home local
+export PATH=~/.local/bin:$PATH
+
+# ARM GNU Toolchain
+export PATH=/Applications/ArmGNUToolchain/12.3.rel1/arm-none-eabi/bin:$PATH
+
+# JDK
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-21-oracle-java-se.jdk/Contents/Home
